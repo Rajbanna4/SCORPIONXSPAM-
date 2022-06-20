f3e72f5dfe9548c3e82de2c1160cfc0ca6a1f952
Raj076 <104069013+Raj076@users.noreply.github.com>
2022-06-20 15:48:16 +0530

    Update utils.py

--- SCORPIONXSpam/utils.py
+++ SCORPIONXSpam/utils.py
@@ -10,8 +10,8 @@ def load_plugins(plugin_name):
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["MKXSpam.plugins." + plugin_name] = load
-    print("MK X has Imported " + plugin_name)
+    sys.modules["SPXSpam.plugins." + plugin_name] = load
+    print("SP X has Imported " + plugin_name)
 
 async def edit_or_reply(event, text):
     if event.sender_id in SUDO_USERS:
